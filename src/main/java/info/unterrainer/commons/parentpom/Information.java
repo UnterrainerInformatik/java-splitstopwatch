9bf0627c96ba06910ee64a403d5a618e0c6d9086
Gerald Unterrainer <gu_falcon@hotmail.com>
2020-12-13 23:20:59 +0100

    fixes

---
+++ src/main/java/info/unterrainer/commons/parentpom/Information.java
@@ -0,0 +1,7 @@
+package info.unterrainer.commons.parentpom;
+
+public class Information {
+	public static final String name = "Parent-POM";
+	public static final String buildTime = "2020-12-13T22:20:41Z";
+	public static final String pomVersion = "0.3.2";
+}
